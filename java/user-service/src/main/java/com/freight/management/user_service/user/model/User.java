6dbb5f2eb1699f570b0e50a12e11970b240d0d5f
cargoproject012 <cargoproject86@gmail.com>
2026-02-18 03:47:23 +0800

    user test

---
+++ java/user-service/src/main/java/com/freight/management/user_service/user/model/User.java
@@ -0,0 +1,26 @@
+package com.freight.management.user_service.user.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "app_user")
+@Data                   // Generates getters, setters, toString, equals, hashCode
+@NoArgsConstructor      // Generates no-args constructor
+@AllArgsConstructor     // Generates all-args constructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
