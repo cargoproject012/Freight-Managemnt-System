62176a88ee00393e5ca467cc451c8854d1e934b5
cargoproject012 <cargoproject86@gmail.com>
2026-02-18 00:04:11 +0800

    d

--- java/config-service/src/main/java/com/freight/management/config_service/ConfigServiceApplication.java
+++ java/config-service/src/main/java/com/freight/management/config_service/ConfigServiceApplication.java
@@ -3,6 +3,7 @@ package com.freight.management.config_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ConfigServiceApplication {
 
